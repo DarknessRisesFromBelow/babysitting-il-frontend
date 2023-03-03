768ab027c9868b8ef520028a89632a5121442aa5
YairMedina <99030228+DarknessRisesFromBelow@users.noreply.github.com>
2023-03-03 12:10:06 +0200

    switched to api.yairmedina.cf

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -8,7 +8,7 @@ import React, {useEffect} from 'react'
 import { Redirect, Route, NavLink, useHistory} from "react-router-dom";
 const fetch = require("cross-fetch");
 
-global.ip = "babysittingil.com";
+global.ip = "api.yairmedina.cf:8080";
 
 const LoginScreen : React.FC = () => {
 const history = useHistory();
